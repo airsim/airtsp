7e7003dab469f251976fcc5caf6d3fcf5228848f
Anh Quan Nguyen <quannaus@users.sourceforge.net>
2010-01-06 16:51:02 +0000

    Small adaptation to stdair.

--- airsched/test/airsched/AirlineScheduleTestSuite.cpp
+++ airsched/test/airsched/AirlineScheduleTestSuite.cpp
@@ -21,6 +21,7 @@
 #include <stdair/bom/SegmentCabin.hpp>
 #include <stdair/bom/LegCabin.hpp>
 #include <stdair/bom/BookingClass.hpp>
+#include <stdair/bom/Network.hpp>
 #include <stdair/bom/BomList.hpp>
 #include <stdair/bom/BomMap.hpp>
 #include <stdair/bom/AirlineFeatureSet.hpp>

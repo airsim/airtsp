b1c47320cc1375ca9f33615391bc915e817b8481
Denis Arnaud <denis@users.sourceforge.net>
2010-01-07 22:00:14 +0000

    [Dev] Fixed a compilation error due to a malformed Makefile (sources.mk).

--- airsched/airsched/command/sources.mk
+++ airsched/airsched/command/sources.mk
@@ -1,9 +1,9 @@
 cmd_h_sources = $(top_srcdir)/airsched/command/CmdAbstract.hpp \
 				$(top_srcdir)/airsched/command/Simulator.hpp \
-				$(top_srcdir)/airsched/command/FileMgr.hpp  \
+				$(top_srcdir)/airsched/command/FileMgr.hpp \
 				$(top_srcdir)/airsched/command/ScheduleParser.hpp \
-				$(top_srcdir)/airsched/command/ScheduleParserHelper.hpp \ \
-				$(top_srcdir)/airsched/command/NetworkGenerator.hpp 
+				$(top_srcdir)/airsched/command/ScheduleParserHelper.hpp \
+				$(top_srcdir)/airsched/command/NetworkGenerator.hpp \
 				$(top_srcdir)/airsched/command/InventoryGenerator.hpp
 cmd_cc_sources = $(top_srcdir)/airsched/command/CmdAbstract.cpp \
 				$(top_srcdir)/airsched/command/Simulator.cpp \

7f5f1b88634f3a7b586e5b4d3888d3a287dbe338
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-03-28 20:50:29 +0200

    [Dev] Standardised the AirSched service (there is still plenty of work to be done on the csvDisplay() and buildSampleBom() methods).

--- airsched/bom/ReachableUniverse.cpp
+++ airsched/bom/ReachableUniverse.cpp
@@ -3,13 +3,30 @@
 // //////////////////////////////////////////////////////////////////////
 // STL
 #include <cassert>
-// STDAIR
+#include <sstream>
+// Boost.Serialization
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/access.hpp>
+// StdAir
 #include <stdair/basic/BasConst_Inventory.hpp>
-// AIRSCHED
+// AirSched
 #include <airsched/bom/ReachableUniverse.hpp>
 #include <airsched/bom/SegmentPathPeriod.hpp>
 
 namespace AIRSCHED {
+
+  // ////////////////////////////////////////////////////////////////////
+  ReachableUniverse::ReachableUniverse()
+    : _key (stdair::DEFAULT_ORIGIN), _parent (NULL) {
+    assert (false);
+  }
+
+  // ////////////////////////////////////////////////////////////////////
+  ReachableUniverse::ReachableUniverse (const ReachableUniverse&)
+    : _key (stdair::DEFAULT_ORIGIN), _parent (NULL) {
+    assert (false);
+  }
 
   // ////////////////////////////////////////////////////////////////////
   ReachableUniverse::ReachableUniverse (const Key_T& iKey)
@@ -17,7 +34,7 @@ namespace AIRSCHED {
   }
 
   // ////////////////////////////////////////////////////////////////////
-  ReachableUniverse::~ReachableUniverse () {
+  ReachableUniverse::~ReachableUniverse() {
   }
 
   // ////////////////////////////////////////////////////////////////////
@@ -51,5 +68,23 @@ namespace AIRSCHED {
     lSegmentPathPeriodLightList.push_back (&iSegmentPathPeriod);
   }
   
+  // ////////////////////////////////////////////////////////////////////
+  void ReachableUniverse::serialisationImplementation() {
+    std::ostringstream oStr;
+    boost::archive::text_oarchive oa (oStr);
+    oa << *this;
+
+    std::istringstream iStr;
+    boost::archive::text_iarchive ia (iStr);
+    ia >> *this;
+  }
+
+  // ////////////////////////////////////////////////////////////////////
+  template<class Archive>
+  void ReachableUniverse::serialize (Archive& ioArchive,
+                                     const unsigned int iFileVersion) {
+    //ioArchive & _key;
+  }
+
 }
 
--- airsched/bom/ReachableUniverse.hpp
+++ airsched/bom/ReachableUniverse.hpp
@@ -4,13 +4,24 @@
 // //////////////////////////////////////////////////////////////////////
 // Import section
 // //////////////////////////////////////////////////////////////////////
-// STDAIR 
+// STL
+#include <iosfwd>
+#include <string>
+// StdAir 
 #include <stdair/bom/BomAbstract.hpp>
-// AIRSCHED
+// AirSched
 #include <airsched/bom/ReachableUniverseKey.hpp>
 #include <airsched/bom/ReachableUniverseTypes.hpp>
 #include <airsched/bom/SegmentPathPeriodTypes.hpp>
 
+/// Forward declarations
+namespace boost {
+  namespace serialization {
+    class access;
+  }
+}
+
+/// Forward declarations
 namespace stdair {
   template <typename BOM> class FacBom;
   class FacBomManager;
@@ -18,74 +29,162 @@ namespace stdair {
 
 namespace AIRSCHED {
 
-  /** Class representing the actual attributes for an airport-date. */
+  /**
+   * @brief Class representing the root of the schedule-related BOM tree.
+   */
   class ReachableUniverse : public stdair::BomAbstract {
     template <typename BOM> friend class stdair::FacBom;
-    friend  class stdair::FacBomManager;
+    friend class stdair::FacBomManager;
+    friend class boost::serialization::access;
 
   public:
-    // Type definitions.
-    /** Definition allowing to retrieve the associated BOM key type. */
+    // ////////// Type definitions ////////////
+    /**
+     * Definition allowing to retrieve the associated BOM key type.
+     */
     typedef ReachableUniverseKey Key_T;
 
   public:
     // /////////// Getters /////////////
-    /** Get the airport-date key. */
-    const Key_T& getKey() const { return _key; }
+    /**
+     * Get the universe key (airport code representing the departure
+     * point of the "reachable universe").
+     */
+    const Key_T& getKey() const {
+      return _key;
+    }
 
-    /** Get the parent object. */
-    stdair::BomAbstract* const getParent() const { return _parent; }
+    /**
+     * Get the parent object.
+     */
+    stdair::BomAbstract* const getParent() const {
+      return _parent;
+    }
     
-    /** Get the (origin) airport (part of the primary key). */
+    /**
+     * Get the (origin) airport (part of the primary key).
+     */
     const stdair::AirportCode_T& getOrigin() const {
       return _key.getBoardingPoint();
     }
 
-    /** Get the SegmentPathPeriodListList. */
-    const SegmentPathPeriodListList_T& getSegmentPathPeriodListList () const {
+    /**
+     * Get the SegmentPathPeriodListList.
+     */
+    const SegmentPathPeriodListList_T& getSegmentPathPeriodListList() const {
       return _segmentPathPeriodListList;
     }
 
-    /** Get the map of children holders. */
-    const stdair::HolderMap_T& getHolderMap() const { return _holderMap; }
+    /**
+     * Get the map of children holders.
+     */
+    const stdair::HolderMap_T& getHolderMap() const {
+      return _holderMap;
+    }
+
 
   public:
     // /////////// Display support methods /////////
-    /** Dump a Business Object into an output stream.
-        @param ostream& the output stream. */
-    void toStream (std::ostream& ioOut) const { ioOut << toString(); }
+    /**
+     * Dump a Business Object into an output stream.
+     *
+     * @param ostream& the output stream.
+     */
+    void toStream (std::ostream& ioOut) const {
+      ioOut << toString();
+    }
 
-    /** Read a Business Object from an input stream.
-        @param istream& the input stream. */
-    void fromStream (std::istream& ioIn) { }
+    /**
+     * Read a Business Object from an input stream.
+     *
+     * @param istream& the input stream.
+     */
+    void fromStream (std::istream& ioIn) {
+    }
 
-   /** Get the serialised version of the Business Object. */
+    /**
+     * Get the serialised version of the Business Object.
+     */
     std::string toString() const;
     
-    /** Get a string describing the  key. */
-    const std::string describeKey() const { return _key.toString(); }
+    /**
+     * Get a string describing the  key.
+     */
+    const std::string describeKey() const {
+      return _key.toString();
+    }
     
+
   public:
     // //////////// Business Methods //////////////
-    /** Add a segment path period to the dedicated list. **/
+    /**
+     * Add a segment path period to the dedicated list.
+     */
     void addSegmentPathPeriod (const SegmentPathPeriod&);    
            
-  protected:
-    /** Default constructors. */
-    ReachableUniverse (const Key_T&);
-    ReachableUniverse (const ReachableUniverse&);
-    /** Destructor. */
-    ~ReachableUniverse();
+
+  public:
+    // /////////// (Boost) Serialisation support methods /////////
+    /**
+     * Serialisation.
+     */
+    template<class Archive>
+    void serialize (Archive& ar, const unsigned int iFileVersion);
+
+  private:
+    /**
+     * Serialisation helper (allows to be sure the template method is
+     * instantiated).
+     */
+    void serialisationImplementation();
+
 
   protected:
-    // Attributes
+    // ////////// Constructors and destructors /////////
+    /**
+     * Default constructor.
+     */
+    ReachableUniverse (const Key_T&);
+
+    /**
+     * Destructor.
+     */
+    ~ReachableUniverse();
+
+  private:
+    /**
+     * Default constructor.
+     */
+    ReachableUniverse();
+
+    /**
+     * Copy constructor.
+     */
+    ReachableUniverse (const ReachableUniverse&);
+
+
+  protected:
+    // ////////// Attributes /////////
+    /**
+     * Primary key (origin airport code).
+     */
     Key_T _key;
+
+    /**
+     * Pointer on the parent class (BomRoot).
+     */
     stdair::BomAbstract* _parent;
+
+    /**
+     * Map holding the children ( objects).
+     */
     stdair::HolderMap_T _holderMap;
     
-    /** The list of lists of SegmentPathPeriods, used uniquement for the
-        construction of the main list of SegmentPathPeriods in
-        ReachableUniverseStructure. */
+    /**
+     * The list of lists of SegmentPathPeriods, used solely for the
+     * construction of the main list of SegmentPathPeriods within the
+     * ReachableUniverseStructure.
+     */
     SegmentPathPeriodListList_T _segmentPathPeriodListList;
   };
 

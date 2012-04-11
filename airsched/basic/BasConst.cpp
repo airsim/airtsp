2452943f21b609e715a7c1b76ab77753e4bdb482
Gabrielle Sabatier <gabrielle.sabatier@amadeus.com>
2012-04-11 15:22:23 +0200

    [Dev] The Segment Path provider does not propose flights too close (or after) request date time.

--- airsched/basic/BasConst.cpp
+++ airsched/basic/BasConst.cpp
@@ -7,4 +7,8 @@ namespace AIRSCHED {
       Integration algorithm. */
   const int DEFAULT_NUMBER_OF_DRAWS_FOR_MC_SIMULATION = 100000;
 
+  /** Default value for the minimum time between the request and the
+      departure time. */
+  const stdair::Duration_T MINIMUM_TIME_BETWEEN_REQUEST_AND_DEPARTURE (4, 0, 0);
+
 }
